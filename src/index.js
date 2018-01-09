0adbcd40a6ef4a03a53d9b521436b0bef2a3768a
Jesse Packwood <Jesse@Jesse.local>
2018-01-09 00:54:30 -0700

    hover states for filter buttons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
-import App from './components/App/App.js';
+import App from './containers/App/App.js';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
