95f2a6a7e1dd08d9553d48f74ce1b5167fe38853
jessepackwood <packwoodjesse@gmail.com>
2018-06-28 08:56:54 -0600

    update portfolio coin name

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -3,7 +3,6 @@ import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import * as actions from '../../actions'
 import PropTypes from 'prop-types'
-
 import './Login.css'
 
 
